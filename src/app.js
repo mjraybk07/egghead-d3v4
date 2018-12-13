cb0296a219e250dcd12ed8bc00236170d797f479
Michael Flis <michaelflis07@gmail.com>
2018-12-13 13:56:50 -0800

    adds svg graphics containers text and transforms location of rects

--- src/app.js
+++ src/app.js
@@ -37,19 +37,44 @@ var scores = [
   
   // Output SVG Elements with D3 v4
   
-d3.select('.chart')
+// d3.select('.chart')
+//   .append('svg')
+//     .attr('width', 225)
+//     .attr('height', 300)
+//   .selectAll('rect')
+//   .data(scores)
+//   .enter()
+//     .append('rect')
+//     .attr('y', (d, i) => i * 33)
+//     .text( function (d) {
+//       return  d.name;
+//     })
+//     .style('width', d => d.score)
+//     .attr('class', 'bar')
+  
+  
+  
+// SVG Graphics Containers and Text Elements in D3 v4
+
+let bar = d3.select('.chart')
   .append('svg')
     .attr('width', 225)
     .attr('height', 300)
-  .selectAll('rect')
+  .selectAll('g')
   .data(scores)
   .enter()
-    .append('rect')
-    .attr('y', (d, i) => i * 33)
-    .text( function (d) {
-      return  d.name;
-    })
-    .style('width', d => d.score)
-    .attr('class', 'bar')
+    .append('g')
+    .attr('transform', (d, i) => 'translate(0,' + i * 33 + ')')
+    
+bar.append('rect')
+  .style('width', d => d.score)
+  .attr('class', 'bar')
+  
+bar.append('text')
+  .attr('y', 20)   
+  .text( function (d) {
+    return  d.name;
+  })
+
   
   
