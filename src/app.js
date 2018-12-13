1e4470b2839f25e4b2762272f6aa197d4e253b0d
Michael Flis <michaelflis07@gmail.com>
2018-12-13 13:38:09 -0800

    adds svg graphics containers

--- src/app.js
+++ src/app.js
@@ -11,25 +11,45 @@ var scores = [
 ];
 
 
-var update = d3.select('.chart')
-  .selectAll('div')
-  .data(scores, function (d) {
-    return d ? d.name : this.innerText
-  })
-  .style('color', 'blue')
+// var update = d3.select('.chart')
+//   .selectAll('div')
+//   .data(scores, function (d) {
+//     return d ? d.name : this.innerText
+//   })
+//   .style('color', 'blue')
   
-let enter = update.enter()
-  .append('div')
-  .text( function (d) {
-    return  d.name;
-  })
-  .style('color', 'green');
+// let enter = update.enter()
+//   .append('div')
+//   .text( function (d) {
+//     return  d.name;
+//   })
+//   .style('color', 'green');
   
   
-update.exit().remove();
+// update.exit().remove();
 
-update.merge(enter)
-  .style('width', d => d.score + 'px')
-  .style('height', '50px')
-  .style('background', 'lightgreen')
-  .style('border', '1px solid black')
+// update.merge(enter)
+//   .style('width', d => d.score + 'px')
+//   .style('height', '50px')
+//   .style('background', 'lightgreen')
+//   .style('border', '1px solid black')
+  
+  
+  // Output SVG Elements with D3 v4
+  
+d3.select('.chart')
+  .append('svg')
+    .attr('width', 225)
+    .attr('height', 300)
+  .selectAll('rect')
+  .data(scores)
+  .enter()
+    .append('rect')
+    .attr('y', (d, i) => i * 33)
+    .text( function (d) {
+      return  d.name;
+    })
+    .style('width', d => d.score)
+    .attr('class', 'bar')
+  
+  
