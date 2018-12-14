f7060acf67db65dc886d5540856db1205cf0cd24
Michael Flis <michaelflis07@gmail.com>
2018-12-14 15:15:59 -0800

    adds labeled scatterplot with health data

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
- 
-//Margin Convention with D3 v4
- 
+
 var margin = { top: 10, right: 20, bottom: 30, left: 30 };
 var width = 400 - margin.left - margin.right;
 var height = 565 - margin.top - margin.bottom;
@@ -13,30 +11,68 @@ var svg = d3.select('.chart')
     .call(responsivefy)
   .append('g')
     .attr('transform', `translate(${margin.left}, ${margin.top})`)
+   
+  
+// load sample data
+d3.json('./data.json', function (err, data) {
+  
+  var yScale = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.expectancy))
+    .range([height, 0])
+    .nice();
     
+  var yAxis = d3.axisLeft(yScale);
+
+  svg.call(yAxis);
+
+
+  var xScale = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.cost))
+    .range([0, width])
+    .nice()
+
+  var xAxis = d3.axisBottom(xScale)
+    .ticks(5)
+
+  svg.append('g')
+      .attr('transform', `translate(0, ${height})`)
+    .call(xAxis)
+    .selectAll('text')
+
+
+  // create circles for charting data
+  var rScale = d3.scaleSqrt()
+    .domain([0, d3.max(data, d => d.population)])
+    .range([0, 40]);
+    
+  var circles = svg.selectAll('.ball')
+    .data(data)
+    .enter()
+    .append('g')
+      .attr('class', 'ball')
+      .attr('transform', d => {
+        return `translate(${xScale(d.cost)}, ${yScale(d.expectancy)})`
+      });
+      
+  circles
+    .append('circle')    
+      .attr('cx', 0)
+      .attr('cy', 0)
+      .attr('r', d => rScale(d.population))
+      .style('fill', 'steelblue')
+      .style('fill-opacity', 0.5);
+      
+  circles
+    .append('text')
+    .style('text-anchor', 'middle')
+    .style('fill', 'black')
+    .attr('y', 4)
+    .text(d => d.code)
   
-var yScale = d3.scaleLinear()
-  .domain([0, 100])
-  .range([height, 0])
-  .nice();
+}) 
+   
+// create axes 
   
-var yAxis = d3.axisLeft(yScale);
-
-svg.call(yAxis);
-
-
-var xScale = d3.scaleLinear()
-  .domain([0, 100])
-  .range([0, width])
-  .nice()
-
-var xAxis = d3.axisBottom(xScale)
-  .ticks(5)
-
-svg.append('g')
-    .attr('transform', `translate(0, ${height})`)
-  .call(xAxis)
-  .selectAll('text')
 
   
 
